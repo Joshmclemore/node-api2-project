aab9abc0fdb2de170395ebf96a22e82bdfc47e68
Joshmclemore <josh_mclemore@yahoo.com>
2022-04-12 16:40:42 -0500

    initialized posts-router

--- api/server.js
+++ api/server.js
@@ -3,9 +3,13 @@
 
 const express = require('express')
 
+const postsRouter = require('./posts/posts-router')
+
 const server = express()
 
 server.use(express.json())
+
+server.use('/api/posts', postsRouter)
 
 server.use('*', (req, res) => {
     res.status(404).json({
